03491048f269f9c2709581ed8994c273ce0eff84
Ryan Boris <ryan.boris@gmail.com>
2019-08-25 18:42:00 -0400

    🚧 Create boilerplate for simple server and views

---
+++ app.js
@@ -0,0 +1,34 @@
+// Simple Express Framework Node Server
+const express = require('express')
+const app = express()
+// Port to listen on
+const PORT = 3000
+
+const path = require('path')
+
+// Custom templating engine
+app.set('view engine', 'pug')
+
+app.set('views', path.resolve('./src/views'))
+
+// Parsing
+app.use(express.json())
+// Create Express Router
+const router = express.Router()
+app.use(router)
+
+const rootPath = path.resolve('./dist')
+app.use(express.static(rootPath))
+
+router.use((err, req, res, next) => {
+  if (err) {
+    return res.send(err.message)
+  }
+})
+
+app.listen(PORT, err => {
+  if (err) {
+    return console.log(`Cannot listen on PORT: ${PORT}`)
+  }
+  console.log(`>>Server is listening on: http://localhost:${PORT}`)
+})
